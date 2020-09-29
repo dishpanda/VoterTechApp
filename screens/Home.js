518e521ce6d0298c81c89050ff9d13dd4efa1a07
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-09-29 02:34:14 -0700

    update homepage to show votertech logo

--- screens/Home.js
+++ screens/Home.js
@@ -2,11 +2,14 @@ import * as React from "react";
 import { Text, View } from "react-native";
 
 function HomeScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Home!</Text>
-      </View>
-    );
-  }
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontWeight: "bold", color: "#003057", fontSize: 50 }}>
+        Voter
+        <Text style={{ color: "#EAAA00" }}>Tech</Text>
+      </Text>
+    </View>
+  );
+}
 
 export default HomeScreen;
