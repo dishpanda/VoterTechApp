f4dd5e7af167ca9fd96f22495ad84882378f18f5
hreddy9 <70776317+hreddy9@users.noreply.github.com>
2020-10-05 17:35:45 -0400

    changed tint color

--- components/Rectangle.js
+++ components/Rectangle.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
     containeri: {
         width:"100%",
         height: "100%",
-        tintColor: "",
+        tintColor: "blue",
     },
     innertext: {
         marginTop: "1%",
