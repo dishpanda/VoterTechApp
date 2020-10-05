3214b5505aa457dcf2ef7a410ddb5e0ebd8d7b76
Sarah Jiwoo Bae <sbae@sarahs-air.attlocal.net>
2020-10-05 15:07:32 -0400

    Fixed launch bug
    metro.config.js

--- babel.config.js
+++ babel.config.js
@@ -4,19 +4,3 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
   };
 };
-
-const { getDefaultConfig } = require("metro-config");
- 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts } 
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-sass-transformer")
-    },
-    resolver: {
-      sourceExts: [...sourceExts, "scss", "sass"]
-    }
-  };
-})();
