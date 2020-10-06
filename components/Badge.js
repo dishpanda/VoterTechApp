8c9fdde1e5bf5e473e8264eeb064969f6f93f5ee
sanabaig98 <56133633+sanabaig98@users.noreply.github.com>
2020-10-06 10:23:15 -0400

    add badge component

---
+++ components/Badge.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Button } from 'react-native-paper';
+
+const BadgeTest = () => (
+  <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
+);
+export default BadgeTest;
--- screens/Game.js
+++ screens/Game.js
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { Text, View } from "react-native";
+import { Badge } from "../components/Components"
 
 function GameScreen() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Game!</Text>
+        <BadgeTest>
+          </BadgeTest>
       </View>
     );
   }
