0e0006d998d6e644c558b9ee5cc1d39c181ca23d
sanabaig98 <56133633+sanabaig98@users.noreply.github.com>
2020-10-06 14:41:26 -0400

    export badge component

--- components/Badge.js
+++ components/Badge.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { Button } from 'react-native-paper';
+import { Button } from "react";
 
-const BadgeTest = () => (
+const Badge = () => (
   <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
     Press me
   </Button>
 );
-export default BadgeTest;
+export default Badge;
--- components/Components.js
+++ components/Components.js
@@ -1,2 +1,3 @@
 export { default as MyTabs } from "./BottomNav";
 export { default as Timer} from "./Timer"
+export {default as Badge} from "./Badge";
