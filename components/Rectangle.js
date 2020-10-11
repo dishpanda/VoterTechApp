6399e77253a3bd695dae76bc0c6679038d01e900
hreddy9 <70776317+hreddy9@users.noreply.github.com>
2020-10-11 17:56:38 -0400

    Update Rectangle.js
    added links to page

--- components/Rectangle.js
+++ components/Rectangle.js
@@ -1,28 +1,33 @@
-import * as React from "react";
-import { StyleSheet, View, Text, Image, ImageBackground} from 'react-native';
+import React, { Component} from 'react'
+import { StyleSheet, View, Text, ImageBackground, Linking} from 'react-native';
+
 
 function Rectangle1() {
     return (
         <View style={styles.container}>
-       
-            <View style={styles.Rectangle}>
+            
+            <View style={styles.Rectangle} onPress={() => Linking.openURL('https://www.vote.org/am-i-registered-to-vote/')}>
                 <ImageBackground source ={require("../assets/ballot.jpg")} style ={styles.containeri}>
                 <View style={{ justifyContent: "flex-start", alignItems: "center" }}> 
-                    <Text style={styles.innertext}>Check Voter Elligibility</Text>
+                    <Text style={styles.innertext}
+                    onPress={() => Linking.openURL('https://www.vote.org/am-i-registered-to-vote/')}>
+                        Check Voter Elligibility</Text>
                 </View> 
                 </ImageBackground>
             </View>
             <View style={styles.Rectangle}>
                 <ImageBackground source ={require("../assets/vbox.jpg")} style ={styles.containeri}>
                 <View style={{ justifyContent: "flex-start", alignItems: "center" }}> 
-                    <Text style={styles.innertext}>Find a Polling Place</Text>
+                    <Text style={styles.innertext}
+                    onPress={() => Linking.openURL("https://www.mvp.sos.ga.gov/MVP/mvp.do")}>Find a Polling Place</Text>
                 </View> 
                 </ImageBackground>
             </View>
             <View style={styles.Rectangle}>
                 <ImageBackground source ={require("../assets/voting.jpg")} style ={styles.containeri}>
                 <View style={{ justifyContent: "flex-start", alignItems: "center" }}> 
-                    <Text style={styles.innertext}>Request Absentee Ballot!</Text>
+                    <Text style={styles.innertext}
+                    onPress={() => Linking.openURL("https://ballotrequest.sos.ga.gov/")}>Request Absentee Ballot!</Text>
                 </View> 
                 </ImageBackground>
             </View>
