95fa7503ef2b8d8cfc2d97479d313805767e5da1
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-10-12 00:57:31 -0700

    Add TopHeader component.
    To use, use `<TopHeader my="true"/>` for "MyVoterTech" to display, and the same with my="false" for displaying just "VoterTech".

---
+++ components/TopHeader.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text, ImageBackground, Linking } from "react-native";
+
+const TopHeader = (props) => {
+  if (props.my == "true") {
+    return (
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <Text
+          style={{
+            fontWeight: "bold",
+            color: "#003057",
+            fontSize: 50,
+            justifyContent: "top",
+          }}
+        >
+          MyVoter
+          <Text style={{ color: "#EAAA00" }}>Tech</Text>
+        </Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <Text
+          style={{
+            fontWeight: "bold",
+            color: "#003057",
+            fontSize: 50,
+            justifyContent: "top",
+          }}
+        >
+          Voter
+          <Text style={{ color: "#EAAA00" }}>Tech</Text>
+        </Text>
+      </View>
+    );
+  }
+};
+
+export default TopHeader;
