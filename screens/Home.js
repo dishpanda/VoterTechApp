ed03e041b4201aa3e865dbad403e30f980f4f326
Sarah Jiwoo Bae <sbae@sarahs-air.attlocal.net>
2020-10-12 01:11:43 -0400

    map attempt (doesn't work)

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 import { Timer } from "../components/Components"
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 function HomeScreen() {
   return (
@@ -10,6 +11,16 @@ function HomeScreen() {
         <Text style={{ color: "#EAAA00" }}>Tech</Text>
       </Text>
       <Timer />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        customMapStyle={MapStyle}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+  />
     </View>
   );
 }
