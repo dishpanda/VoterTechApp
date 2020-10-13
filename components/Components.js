7e83d8173e224e56262296dc301cae8e40c859af
sanabaig98 <56133633+sanabaig98@users.noreply.github.com>
2020-10-13 00:55:07 -0400

    Add icons for two badges

--- components/Components.js
+++ components/Components.js
@@ -1,9 +1,5 @@
 export { default as MyTabs } from "./BottomNav";
-<<<<<<< HEAD
 export { default as Timer} from "./Timer"
 export { default as Header} from "./Header"
-=======
-export { default as Timer} from "./Timer";
-export { default as Header} from "./Header";
-export { default as TopHeader } from "./TopHeader";
->>>>>>> 9ee04210c5808c40998cead7a47f67917c8251c6
+export { default as Badge} from "./Badge"
+export { default as TopHeader} from "./TopHeader"
--- screens/Game.js
+++ screens/Game.js
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { Header } from "../components/Components";
+import { Text, View } from "react-native";
+import Badge from "../components/Badge"
 
 function GameScreen() {
     return (
-      <Header />
+      <View style={{ flex: 1, justifyContent: "start", alignItems: "center" }}>
+     <Badge></Badge>
+     </View>
     );
   }
 
