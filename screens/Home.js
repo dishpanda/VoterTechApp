88b2460c56f3620195ea0387c0226bfd3a78e25d
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-10-27 03:00:47 -0700

    add outer ring

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,15 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 import { Timer, TopHeader } from "../components/Components";
+import OuterCircle from "../components/OuterCircle";
 
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center" }}>
       <TopHeader my="false" />
-      <span class="Circle"></span>
-      <Timer />
+      <OuterCircle>
+        <Timer />
+      </OuterCircle>
     </View>
   );
 }
